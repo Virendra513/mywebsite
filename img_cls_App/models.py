d78081a60b3b83299388ee7ab433096803ac12e3
Virendra <107255416+Virendra513@users.noreply.github.com>
2024-10-08 23:09:14 +0530

    Delete img_cls_App directory

--- img_cls_App/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from cloudinary.models import CloudinaryField
-
-# Create your models here.
-class MyModel(models.Model):
-    image = CloudinaryField('image')
-
-    # def __str__(self):
-    #     return f"Image: {self.image.url}"
--- img_cls_App/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from img_cls_App import views
-
-urlpatterns = [
-    path('', views.index_img, name='img_classification'),
-    path('predict_img/', views.predict_img, name='predict_img')
-]
